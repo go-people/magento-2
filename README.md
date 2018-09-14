6004d77fa9e84a705fb10ebffb2fe6b683408dda
4DallasC <dallasc@ekkysoftware.com>
2018-09-14 12:42:39 +1000

    Initial check in of standard Magento 2 extension structure

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# magento-2
+The GoPeople_Shipping module provides the models and interfaces for a live carrier integration and pricing.
+You need to extend these abstractions if you are adding new shipping carrier integration.
---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright © 2018 Ekky Software Pty Ltd. All rights reserved.
+ * Please visit http://www.ekkysoftware.com
+ * See COPYING.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'GoPeople_Shipping',
+    __DIR__
+);
