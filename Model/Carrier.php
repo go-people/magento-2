0d002ed033685650cde8141e7fad1155ad716b09
4DallasC <dallasc@ekkysoftware.com>
2018-09-16 10:30:33 +1000

    Adding core shiping method files

---
+++ Model/Carrier.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Copyright © 2018 Ekky Software Pty Ltd. All rights reserved.
+ * Please visit http://www.ekkysoftware.com
+ * See COPYING.txt for license details.
+ */
+
+// @codingStandardsIgnoreFile
+
+namespace GoPeople\Shipping\Model;
+
+use Magento\Quote\Model\Quote\Address\RateResult\Error;
+use Magento\Quote\Model\Quote\Address\RateRequest;
+use Magento\Shipping\Model\Carrier\AbstractCarrierOnline;
+use Magento\Shipping\Model\Carrier\CarrierInterface;
+use Magento\Shipping\Model\Rate\Result;
+use Magento\Shipping\Model\Simplexml\Element;
+
+/**
+ * UPS shipping implementation
+ * @SuppressWarnings(PHPMD.ExcessiveClassComplexity)
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class Carrier extends AbstractCarrierOnline implements CarrierInterface
+{
+    /**
+     * Code of the carrier
+     *
+     * @var string
+     */
+    const CODE = 'gopeople';
+
+    /**
+     * Code of the carrier
+     *
+     * @var string
+     */
+    protected $_code = self::CODE;
+
+    /**
+     * Rate request data
+     *
+     * @var RateRequest
+     */
+    protected $_request;
+
+    /**
+     * Rate result data
+     *
+     * @var Result
+     */
+    protected $_result;
+
+    /**
+     * Collect and get rates/errors
+     *
+     * @param RateRequest $request
+     * @return  Result|Error|bool
+     */
+    public function collectRates(RateRequest $request)
+    {
+        if (!$this->canCollectRates()) {
+            return $this->getErrorMessage();
+        }
+
+        return false;//return no quote
+    }
+
+}
