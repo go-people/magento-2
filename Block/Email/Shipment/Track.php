a14f3d0ad8cdbf5ff4671e89a94b346b1539ee7e
4DallasC <dallasc@ekkysoftware.com>
2018-10-23 16:33:30 +1100

    Adding support for tracking link in new shipment email

---
+++ Block/Email/Shipment/Track.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright © 2018 Ekky Software Pty Ltd. All rights reserved.
+ * Please visit http://www.ekkysoftware.com
+ * See COPYING.txt for license details.
+ */
+
+// @codingStandardsIgnoreFile
+
+namespace GoPeople\Shipping\Block\Email\Shipment;
+
+/**
+ * Go People tracking email implementation
+ */
+class Track
+extends \Magento\Framework\View\Element\Template
+{
+
+    public function getTrackingLink($_item){
+        if($_item->getCarrierCode() == \GoPeople\Shipping\Model\Carrier::CODE){
+            return '<a href="https://www.gopeople.com.au/tracking/?code='.$_item->getNumber().'">'.$this->escapeHtml($_item->getNumber()).'</a>';
+        }
+        return $this->escapeHtml($_item->getNumber());
+    }
+
+}
